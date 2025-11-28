7ed05e520dce58fc3aa61341cffd5e80c372f068
alazaryohannes7241 <alazaryohannes72411@gmail.com>
2025-11-28 23:02:04 +0100

    Initial commit

---
+++ backend/db_helper.py
@@ -0,0 +1,74 @@
+import mysql.connector
+from contextlib import contextmanager
+from logging_setup import setup_logger
+
+logger=setup_logger('db_helper')
+
+
+
+@contextmanager
+def get_db_cursor(commit=False):
+    connection=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='maryam21',
+    database='expense_mana'
+    )
+    if connection.is_connected():
+        print('connection successful')
+    else:
+        print('Failed in connecting')
+
+    cursor=connection.cursor(dictionary=True)
+    yield cursor
+    
+    if commit:
+        connection.commit()
+    
+    cursor.close()
+    connection.close()
+    
+def fetch_all_records():
+    with get_db_cursor() as cursor:
+        cursor.execute('select * from expenses')
+        expenses=cursor.fetchall()
+        for expense in expenses:
+            print(expense)
+    
+    
+def fetch_expenses_for_date(expense_date):
+    logger.info(f'fetch_expenses_for_date called with {expense_date}')
+    with get_db_cursor() as cursor:
+        cursor.execute('select * from expenses where expense_date=%s',(expense_date,))
+        expenses=cursor.fetchall()
+        for expense in expenses:
+            print(expense)
+        return expenses
+    
+def insert_expense(expense_date,amount,category,notes):
+    logger.info(f'insert_expense called with date: {expense_date}, amount: {amount}, category: {category}, notes: {notes}')
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(
+            'insert into expenses (expense_date,amount,category,notes) values (%s, %s, %s, %s)',
+            (expense_date,amount,category,notes)
+        )
+
+def delete_expenses_for_date(expense_date):
+    logger.info(f'fetch_expenses_for_date called with {expense_date}')
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute('delete from expenses where expense_date=%s',(expense_date,))
+        
+def fetch_expense_summary(start_date, end_date):
+    logger.info(f'fetch_expense_summary called with start: {start_date} end: {end_date}')
+    with get_db_cursor() as cursor:
+        cursor.execute(
+            '''select category, sum(amount) as total
+            from expenses where expense_date between %s and %s
+            group by category;''',(start_date,end_date)
+        )
+        data=cursor.fetchall()
+        return data
+
+if __name__=='__main__':
+    expenses=fetch_expenses_for_date('2024-09-30')
+    print(expenses)
---
+++ frontend/README.md
@@ -0,0 +1,31 @@
+# Expense Management System
+
+This project is an expense management system that consists of a streamlit frontend and a FastAPI backend server.
+
+
+## Project Structure
+
+- **frontend/**: Contains the Streamlit application code.
+- **backend/**: Contains the FastAPI backend server code.
+- **requirements.txt**: Lists the required Python packages.
+- **README.MD**: Provides an overview and instructions for the project
+
+## Setup Instructions
+
+1. **Clone the repository**:
+    '''bash
+    git clone https://github.com/yourusername/expense-management-system
+    cd expense-management-system
+    '''
+1. **Install dependencies:**:
+    '''commandline
+    pip install -r requirements.txt
+    '''
+1. **Run the FastAPI server:**:
+    ''' commandline
+    uvicorn server.server:app --reload
+    '''
+1. **Run the Streamlit app:**:
+    ''' commandline
+    stramlit run frontend/app.py
+    '''
